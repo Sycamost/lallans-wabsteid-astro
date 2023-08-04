4dea89fa67bf52fff2c4031a19244823aec7493e
Joe Carstairs <jcarstairs@scottlogic.com>
2023-08-04 18:34:12 +0100

    Defines a default locale

--- frontend/src/i18n/locales.ts
+++ frontend/src/i18n/locales.ts
@@ -6,4 +6,11 @@ const localesHash: { [key in Locale]: 1 } = {
   'en-GB': 1,
 };
 
+// Use the magic of TypeScript to guarantee that the
+// default locale is a locale, but also have const
+// string type
+const defaultLocaleAsLocale: Locale = 'sco';
+const defaultLocale: 'sco' = defaultLocaleAsLocale;
+
+export { defaultLocale };
 export default Object.keys(localesHash) as Locale[];
