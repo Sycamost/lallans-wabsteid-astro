bfdcb0ab536c03d03e0c69cc883f9a2934ec4fb0
Joe Carstairs <jcarstairs@scottlogic.com>
2023-08-04 18:35:06 +0100

    Defines translation function

---
+++ frontend/src/i18n/translate.ts
@@ -0,0 +1,9 @@
+import type Locale from "../types/Locale";
+import type { TranslationsDictionary } from "../types/TranslationsDictionary";
+import { defaultLocale } from "./locales";
+
+export default function(locale: Locale) {
+  return function<T extends string>(dict: TranslationsDictionary<T>, key: T) {
+    return dict[key][locale] ?? dict[key][defaultLocale];
+  }
+}
