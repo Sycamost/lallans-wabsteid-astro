aba5b4e4879712b691532f855eb718d12dd5b9e5
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-08-05 21:32:43 +0100

    Moves localeStaticPaths

--- src/lib/localeStaticPaths.ts
+++ src/lib/localeStaticPaths.ts
@@ -1,10 +1,10 @@
-import type Locale from '../../types/Locale';
+import type Locale from '../types/Locale';
 
 // Defines what values to insert into the [locale] URL path parameter
 // when generating a static site
-const staticPaths: { params: { locale: Locale} }[] = [
+const localeStaticPaths: { params: { locale: Locale} }[] = [
   { params: { locale: 'sco' } },
   { params: { locale: 'en-GB' } },
 ]
 
-export default staticPaths;
+export default localeStaticPaths;
