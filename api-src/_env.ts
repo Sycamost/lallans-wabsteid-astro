2f6f0c8b52a1df369f0b9af25017233287fef9db
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-08-07 21:00:13 +0100

    Fixes env path

--- api-src/_env.ts
+++ api-src/_env.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 // Parsing the env file.
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 type Env = {
   PAYPAL_LIVE_CLIENT_ID: string,
