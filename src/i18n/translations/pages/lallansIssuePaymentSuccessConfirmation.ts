e9755981b06470c495edde3fbfc3934b1bc5c107
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-08-12 17:42:59 +0100

    Adds payment success confirmation page for Lallans issues

---
+++ src/i18n/translations/pages/lallansIssuePaymentSuccessConfirmation.ts
@@ -0,0 +1,26 @@
+import type { TranslationsDictionary } from '../../../types/TranslationsDictionary';
+
+const lallansIssuePaymentSuccessConfirmation = {
+  title: {
+    sco: () => 'Payment Success',
+    'en-GB': () => 'Payment Success',
+  },
+  'payment-confirmation-message': {
+    sco: ({ issueNumber }: { issueNumber: string }) => `
+      Gin ye hae been sent til this page, at means ye hae successfully ordered
+      Lallans ${issueNumber} an we’ll be pittin it in the post fur ye belyve!
+    `,
+    'en-GB': ({ issueNumber }: { issueNumber: string }) => `
+      If you have been directed to this page, that means you have successfully
+      ordered Lallans ${issueNumber} and we shall be putting it in the post for
+      you right away!
+    `,
+  },
+  'back-button-text': {
+    sco: ({ issueNumber }: { issueNumber: string }) => `Retour til Lallans ${issueNumber}`,
+    'en-GB': ({ issueNumber }: { issueNumber: string }) => `Return to Lallans ${issueNumber}`,
+  },
+};
+
+type Raw = typeof lallansIssuePaymentSuccessConfirmation;
+export default lallansIssuePaymentSuccessConfirmation as TranslationsDictionary<Raw>;
