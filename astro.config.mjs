5839f40745cbfd8945fd31327dc61ca147029b51
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-08-24 22:31:29 +0100

    Adds sitemap for lallans.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
+  site: 'https://www.lallans.co.uk',
 });
