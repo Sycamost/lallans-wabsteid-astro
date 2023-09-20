c24f646daf9a503531baea046a962f2fd0478a8e
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-09-20 21:04:39 +0100

    Adds a description to Scotsoun

--- src/i18n/translations/pages/scotsounGallery.ts
+++ src/i18n/translations/pages/scotsounGallery.ts
@@ -5,6 +5,16 @@ const tPage = {
     sco: () => 'Scotsoun',
     'en-GB': () => 'Scotsoun',
   },
+  'about-scotsoun': {
+    sco: () => `
+      Scotsoun is oor lang-rinnin series o Scots-leid audio recordins, i the form o tales,
+      drama, sangs, an poesie readins. Forby, we hae some muisic CDs.
+    `,
+    'en-GB': () => `
+      Scotsoun is our long-running series of Scots-language audio recordings, in the form
+      of stories, drama, songs and poetry readings. We also have some music CDs.
+    `,
+  },
 };
 
 type Raw = typeof tPage;
