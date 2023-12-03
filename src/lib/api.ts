0884e05c48d1a7b0f5c22f475c6b643055c8f5fa
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-12-03 09:08:10 +0000

    Can access authentication API functions on frontend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,8 @@
+import type {
+  PublicKeyCredentialCreationOptionsJSON,
+  RegistrationResponseJSON,
+} from '@simplewebauthn/typescript-types';
+
 const backendUrl = '/api';
 
 export async function captureOrder(orderId: string) {
@@ -29,4 +34,37 @@ type CreateOrderParams = {
   productDescription: string;
   shortDescription: string;
   totalPrice: string;
+};
+
+export async function getRegistrationOptions(params: GetRegistrationOptionsParams) {
+  const queryString = new URLSearchParams(params).toString();
+  // If last param ends with, e.g., `.com` as in an email address, the `.com`
+  // will be interpreted as a file extension, and this will break everything.
+  // Hack to avoid this bug.
+  return await fetch(`${backendUrl}/auth/registration?${queryString}&hack=toavoidbug`, {
+    method: 'GET',
+    body: JSON.stringify(params),
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+type GetRegistrationOptionsParams = {
+  userId: string;
+  displayName: string;
+};
+
+export type GetRegistrationOptionsResponseBody = PublicKeyCredentialCreationOptionsJSON;
+
+export async function verifyRegistrationResponse(params: VerifyRegistrationResponseParams) {
+  return await fetch(`${backendUrl}/auth/registration`, {
+    method: 'POST',
+    body: JSON.stringify(params),
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+type VerifyRegistrationResponseParams = {
+  userId: string;
+  displayName: string;
+  registrationResponse: RegistrationResponseJSON;
 };
