cdb0e90d97b02fde0971ca3332606ad369bac315
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2023-12-24 10:49:40 +0000

    Improves error logging in database queries (#53)

--- api/db/_addAuthenticator.ts
+++ api/db/_addAuthenticator.ts
@@ -4,29 +4,31 @@ import { sql } from '@vercel/postgres';
 import { AUTHENTICATORS } from './_tables';
 
 export default async function addAuthenticator(authenticator: Authenticator) {
+  const query = `
+    INSERT INTO ${AUTHENTICATORS.name} (
+      ${AUTHENTICATORS.fields.id},
+      ${AUTHENTICATORS.fields.backedUp},
+      ${AUTHENTICATORS.fields.counter},
+      ${AUTHENTICATORS.fields.deviceType},
+      ${AUTHENTICATORS.fields.publicKey}
+      ${AUTHENTICATORS.fields.transports},
+      ${AUTHENTICATORS.fields.type},
+      ${AUTHENTICATORS.fields.userId}
+    ) VALUES (
+      ${authenticator.id},
+      ${authenticator.backedUp},
+      ${authenticator.counter},
+      ${authenticator.deviceType},
+      ${authenticator.publicKey}
+      { ${authenticator.transports.join(', ')} },
+      ${authenticator.type},
+      ${authenticator.userId}
+    );
+  `;
+
   try {
-    await sql.query(`
-      INSERT INTO ${AUTHENTICATORS.name} (
-        ${AUTHENTICATORS.fields.id},
-        ${AUTHENTICATORS.fields.backedUp},
-        ${AUTHENTICATORS.fields.counter},
-        ${AUTHENTICATORS.fields.deviceType},
-        ${AUTHENTICATORS.fields.publicKey}
-        ${AUTHENTICATORS.fields.transports},
-        ${AUTHENTICATORS.fields.type},
-        ${AUTHENTICATORS.fields.userId}
-      ) VALUES (
-        ${authenticator.id},
-        ${authenticator.backedUp},
-        ${authenticator.counter},
-        ${authenticator.deviceType},
-        ${authenticator.publicKey}
-        { ${authenticator.transports.join(', ')} },
-        ${authenticator.type},
-        ${authenticator.userId}
-      );
-    `);
+    await sql.query(query);
   } catch (err) {
-    throw new Error(`Failed to insert authenticator into database: ${JSON.stringify(authenticator)}.`, err);
+    throw new Error(`Failed to insert authenticator into database: ${JSON.stringify(authenticator)}. Query was ${query.replace(/\s+/g, ' ')}. ${err}`);
   }
 }
