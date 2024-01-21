aabde3ea11ff75513abe5dabd1931a2b76b116c4
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2024-01-21 10:09:45 +0000

    Fixes sign-up page

--- src/i18n/translations/pages/signUp/index.ts
+++ src/i18n/translations/pages/signUp/index.ts
@@ -17,6 +17,27 @@ const tPage = {
     sco: () => 'Sign up',
     'en-GB': () => 'Sign up',
   },
+  'status-working': {
+    sco: () => 'Signin you up…',
+    'en-GB': () => 'Signing you up…',
+  },
+  'unexpected-error': {
+    sco: () => 'There wis an unexpectit mishanter.',
+    'en-GB': () => 'There was an expected error.',
+  },
+  'error-getting-registration-options': {
+    sco: () => 'There wis a mishanter gettin registration options frae the server.',
+    'en-GB': () => 'There was an error getting registration options from the server.',
+  },
+  'error-starting-registration': {
+    sco: () => 'There wis a mishanter whan your browser tried tae stert registration.',
+    'en-GB': () => 'There was an error when your browser tried to start registration.',
+  },
+  'error-verifying-registration-response': {
+    sco: () =>
+      'There wis a mishanter whan the server tried tae check that the registration response wis suithfest.',
+    'en-GB': () => 'There was an error when the server tried to verify the registration response.',
+  },
 };
 
 type Raw = typeof tPage;
