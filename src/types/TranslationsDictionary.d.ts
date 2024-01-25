3a0a45ab06ff0bde1705795be4ba97d6836708af
Joe Carstairs <jcarstairs@scottlogic.com>
2024-01-25 11:31:43 +0000

    Reworks translation typing

--- src/types/TranslationsDictionary.d.ts
+++ src/types/TranslationsDictionary.d.ts
@@ -1,18 +1,15 @@
 import type { defaultLocale } from '$i18n/locales';
 import type Locale from './Locale';
 
-export type Translation<Params, TParams> = (params: { [key in keyof Params]: TParams }) => string;
+export type Translation<ParamsHash extends object> = (params: ParamsHash) => string;
+export type LocalesTranslationHash<ParamsHash extends object> = {
+  [locale in Locale]?: Translation<ParamsHash>;
+} & {
+  [locale in typeof defaultLocale]: Translation<ParamsHash>;
+};
 
 export type TranslationsDictionary<Raw> = {
-  readonly [key in keyof Raw]: {
-    [locale in Locale]?: Translation<
-      Parameters<Raw[key][typeof defaultLocale]>[0],
-      Raw[typeof defaultLocale][Parameters<Raw[key][typeof defaultLocale]>[0]]
-    >;
-  } & {
-    [locale in typeof defaultLocale]: Translation<
-      Parameters<Raw[key][typeof defaultLocale]>[0],
-      Raw[typeof defaultLocale][Parameters<Raw[key][typeof defaultLocale]>[0]]
-    >;
-  };
+  readonly [key in keyof Raw]: LocalesTranslationHash<
+    Parameters<Raw[key][typeof defaultLocale]>[0]
+  >;
 };
