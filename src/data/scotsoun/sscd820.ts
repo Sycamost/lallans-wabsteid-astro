527dfcff5082a2bdf51419c75a7286f786af9c35
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2024-03-19 08:21:43 +0000

    Adds SSCD 820 data

---
+++ src/data/scotsoun/sscd820.ts
@@ -0,0 +1,87 @@
+import img192w from '$images/scotsoun/sscd820-192w.jpg';
+import type Scotsoun from '$types/Scotsoun';
+
+const cd: Scotsoun = {
+  img: {
+    width192: img192w,
+  },
+  title: 'Let the thochts gae roamin',
+  longName: 'A vision o ma ain countree – Let the thochts gae roamin',
+  author: 'various',
+  cdCount: 1,
+  price: '10.00',
+  scotsounId: '820',
+  uploadDate: '2024-03-19',
+  trackList: [
+    'Kate YA Bone – Some Ghaists Haunt Housses',
+    'Kate YA Bone – The Last Fire',
+    'Kate YA Bone – Prayer to Saint Andrew',
+
+    'Muriel Ferrier – Howff',
+    'Muriel Ferrier – Saumen Rin',
+
+    'Tracy Ann Harvey – Breeks',
+    'Tracy Ann Harvey – It’s No Potata it’s Tatties',
+    'Tracy Ann Harvey – Leanne an Betty Throu the Wa',
+    'Tracy Ann Harvey – Ma Language',
+    'Tracy Ann Harvey – Schuil Dinners',
+    'Tracy Ann Harvey – Seagulls',
+    'Tracy Ann Harvey – Ettlin fur Freedom',
+    'Tracy Ann Harvey – The Ayr Witch',
+    'Tracy Ann Harvey – First Winch',
+    'Tracy Ann Harvey – Frogs',
+
+    'Irene Howat – Granfaither',
+    'Irene Howat – Winter of 47',
+    'Irene Howat – Wrannie',
+    'Irene Howat – Souch o a Poem',
+    'Irene Howat – Nip Nebs',
+
+    'Laureen Johnson – Deep',
+    'Laureen Johnson – Staunin Steen',
+    'Laureen Johnson – March-past at Arromanches',
+
+    'Jean Massie – Hill o the Faeries',
+    'Jean Massie – Balmarino Abbey',
+    'Jean Massie – Wrack an Waith',
+
+    'Edith MacArthur – The Braw Thocht',
+
+    'Anne McMaster – So Many Wurds for Soil',
+    'Anne McMaster – Ghaists',
+    'Anne McMaster – Whenever the Backend of the Year',
+    'Anne McMaster – The Cet Now are Getting Aul',
+    'Anne McMaster – All Life is Breath',
+    'Anne McMaster – Night – To Stert',
+    'Anne McMaster – Thanks',
+    'Anne McMaster – The Last Sang',
+
+    'Eilidh Rut NicBhalteir – The Storm',
+
+    'Dorothy Margaret Paulin – A Galloway Legend',
+    'Dorothy Margaret Paulin – Solway Tide',
+    'Dorothy Margaret Paulin – Wan Watter',
+    'Dorothy Margaret Paulin – Perplexity',
+    'Dorothy Margaret Paulin – Sodger’s Lass',
+
+    'Lynn Valentine – Ma faither at 4am',
+    'Lynn Valentine – Loss of the Westhaven',
+    'Lynn Valentine – The Leid o Hame',
+    'Lynn Valentine – Missin',
+    'Lynn Valentine – A lost Freen',
+    'Lynn Valentine – At the Clootie Well',
+    'Lynn Valentine – Snaw Blind',
+  ],
+  description: {
+    sco: `
+      A collection o some o the best female poets warkin in the Scots leid
+      the day.
+    `,
+    'en-GB': `
+      A collection of some of the best female poets working in the Scots
+      language today.
+    `,
+  },
+};
+
+export default cd;
